03d0dd0165f7cdc3497c74f4753484a7b460c458
Rutger <1201954@student.roc-nijmegen.nl>
2024-02-28 13:39:41 +0100

    Add modifiers

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -1,9 +1,9 @@
 ﻿namespace Blackjack;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello World!");
     }
 }
